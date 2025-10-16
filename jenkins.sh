eacec57f720d3470341a368f39f21c51f9620b59
AJ@Y SHAHU <ajayshahu1104@gmail.com>
2025-10-16 11:06:11 +0530

    Update jenkins.sh for Debian package management

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,15 @@
 
 #! /bin/bash
-sudo yum update -y
-sudo yum install -y openjdk-17-jdk
+sudo apt update -y
+sudo apt install -y openjdk-17-jdk
 java -version
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/yum/sources.list.d/jenkins.list > /dev/null
-sudo yum update -y
-sudo yum install -y jenkins
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install -y jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
